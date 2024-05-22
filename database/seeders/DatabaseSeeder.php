b4eb25d8a78bd139c318f7c3cd735d5141d408ed
mateusz <fl_mateusz.lesniak@towa.at>
2024-05-22 03:39:06 +0200

    Include entity name in seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -37,7 +37,7 @@ class DatabaseSeeder extends Seeder
                     })
                     ->afterCreating(function () use (&$date) {
                         $date = new DateTimeImmutable();
-                    })
+                    }), 'vacancies'
             )
             ->create();
     }
